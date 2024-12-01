0b929e85890a4c12efcb64a6af7acb9e0b546aaf
Görkem Paçacı <gorkempacaci@gmail.com>
2024-12-01 19:18:40 +0100

    day 1

---
+++ day01/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+    Görkem Paçacı
+    2024-12-01 AoC day 1
+*/
+
+List<int> left = new(), right = new();
+foreach(var line in File.ReadAllLines("input.txt"))
+{
+    var lineArr = line.Split("   ").Select(int.Parse).ToArray();
+    left.Add(lineArr[0]);
+    right.Add(lineArr[1]);
+}
+left.Sort();
+right.Sort();
+var sum = left.Zip(right, (l,r)=>Math.Abs(l-r)).Sum();
+Console.WriteLine(sum);
---
+++ day01b/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+    Görkem Paçacı
+    2024-12-01 AoC day 1
+*/
+using System.Collections;
+List<int> left = new(), right = new();
+foreach(var line in File.ReadAllLines("input.txt"))
+{
+    var lineArr = line.Split("   ").Select(int.Parse).ToArray();
+    left.Add(lineArr[0]);
+    right.Add(lineArr[1]);
+}
+left.Sort();
+right.Sort();
+var sum = left.Select(l => l * right.Count(r => r==l)).Sum();
+Console.WriteLine(sum);
