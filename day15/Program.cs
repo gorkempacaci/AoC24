014ab8ab56c2d81f5529563fade57ce7f9ecf777
Görkem Paçacı <gorkempacaci@gmail.com>
2024-12-19 11:12:01 +0100

    day 16

---
+++ day15/Program.cs
@@ -0,0 +1,242 @@
+﻿/*
+    Görkem Paçacı
+    AoC24 day 15
+*/
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+
+Stopwatch sp = new Stopwatch();
+sp.Start();
+//part1();
+sp.Stop();
+Console.WriteLine("time: " + sp.ToString());
+sp.Restart();
+part2();
+sp.Stop();
+Console.WriteLine("time:" + sp.ToString());
+
+// void part1()
+// {
+//     var lines = File.ReadAllLines("input.txt").Select(l => l.ToCharArray()).ToArray();
+//     int linesLength = -1;
+//     (int x, int y) robPos = (-1, -1);
+//     for(int i=0; i<lines.Length; i++)
+//     {
+//         int pos = Array.IndexOf(lines[i], '@');
+//         if (pos > -1)
+//             robPos = (pos, i);
+//         if (lines[i].Length==0)
+//         {
+//             linesLength = i;
+//             break;
+//         }
+//     }
+//     string moves = lines.Skip(linesLength).Select(cs => new string(cs)).Aggregate(string.Concat);
+//     foreach(char move in moves)
+//         push(lines, ref robPos, move);
+//     long totDistance = 0;
+//     for(int i=0; i<linesLength; i++)
+//     {
+//         for(int x=0; x<lines[i].Length; x++)
+//         {
+//             if (lines[i][x]=='O')
+//                 totDistance += i*100+x;
+//         }
+//     }
+//     Console.WriteLine("Part 1:"+totDistance);
+// }
+
+// bool push(char[][] lines, ref (int x, int y) pos, char op)
+// {
+//     (int x, int y) dir = op switch
+//     {
+//         '<' => (-1, 0),
+//         '>' => (1, 0),
+//         'v' => (0, 1),
+//         '^' => (0, -1),
+//         _ => throw new Exception("Not known move:"+op)
+//     };
+//     int xx=pos.x + dir.x, yy=pos.y+dir.y;
+//     if (lines[yy][xx] == '#')
+//         return false;
+//     if (lines[yy][xx] == '.')
+//     {
+//         lines[yy][xx] = lines[pos.y][pos.x];
+//         lines[pos.y][pos.x] = '.';
+//         pos = (xx, yy);
+//         return true;
+//     }
+//     if (lines[yy][xx] == 'O')
+//     {
+//         (int x, int y) dummy = (xx,yy);
+//         if (push(lines, ref dummy, op))
+//         {
+//             lines[yy][xx] = lines[pos.y][pos.x];
+//             lines[pos.y][pos.x] = '.';
+//             pos = (xx, yy);
+//             return true;
+//         } else return false;
+//     }
+//     throw new NotImplementedException("Unhandled target box:" + lines[yy][xx]);
+// }
+
+void part2()
+{
+    var linesI = File.ReadAllLines("input.txt").ToArray();
+    int linesLength = -1;
+    (int x, int y) robPos = (-1, -1);
+    for(int i=0; i<linesI.Length; i++)
+    {
+        if (linesLength == -1)
+        {
+            linesI[i] = linesI[i].Replace(".", "..").Replace("@", "@.").Replace("#","##").Replace("O", "[]");
+        }
+        int pos = linesI[i].IndexOf('@');
+        if (pos > -1)
+            robPos = (pos, i);
+        if (linesI[i].Length==0)
+        {
+            linesLength = i;
+            break;
+        }
+    }
+    char[][] lines = linesI.Select(l => l.ToCharArray()).ToArray();
+    // foreach(var line in lines)
+    //     Console.WriteLine(line);
+    string moves = lines.Skip(linesLength).Select(cs => new string(cs)).Aggregate(string.Concat);
+    printLines(lines, robPos);
+    foreach(char move in moves)
+    {
+        evacuate(lines, ref robPos, move);
+        printLines(lines, robPos);
+    }
+    long totDistance = 0;
+    for(int i=0; i<linesLength; i++)
+    {
+        for(int x=0; x<lines[i].Length; x++)
+        {
+            if (lines[i][x]=='[')
+                totDistance += i*100+x;
+        }
+    }
+    printLines(lines, (0,0));
+    Console.WriteLine("Part 2:"+totDistance);
+}
+
+
+// move whatever in pos in op direction
+bool push2(char[][] lines, ref (int x, int y) pos, char op)
+{
+    char[][] linesBackup = lines.Select(l => new string(l).ToCharArray()).ToArray();
+    (int x, int y) dir = op switch
+    {
+        '<' => (-1, 0),
+        '>' => (1, 0),
+        'v' => (0, 1),
+        '^' => (0, -1),
+        _ => throw new Exception("Not known move:"+op)
+    };
+    int mySO = lines[pos.y][pos.x] switch
+    {
+        '[' => 1,
+        ']' => -1,
+        _ => 0
+    };
+
+    int xx=pos.x + dir.x, yy=pos.y+dir.y;
+    int targetSO = lines[yy][xx]==']'?-1:(lines[yy][xx]=='['?+1:0);
+
+    if (lines[yy][xx] == '.' && lines[yy][xx+mySO] == '.')
+    {
+        lines[yy][xx] = lines[pos.y][pos.x];
+        lines[yy][xx+mySO] = lines[pos.y][pos.x+mySO];
+        lines[pos.y][pos.x] = '.';
+        lines[pos.y][pos.x+mySO] = '.';
+        pos = (xx, yy);
+        return true;
+    }
+
+    if (lines[yy][xx] == '#' || lines[yy][xx+mySO] == '#')
+        return false;
+
+
+
+    if (mySO == 0)
+    {
+        if (evacuate(lines, ref pos, op))
+        {
+            lines[yy][xx] = lines[pos.y][pos.x];
+            return true;
+        } else return false;
+    }
+    else
+    {
+        (int x, int y) dummy = (xx,yy), dummy2 = (xx+mySO,yy);
+        if (evacuate(lines, ref dummy2, op) && evacuate(lines, ref dummy, op))
+        {
+            lines[yy][xx+mySO] = lines[pos.y][pos.x+mySO];
+            lines[yy][xx] = lines[pos.y][pos.x];
+            //lines[pos.y][pos.x+pushSO] = '.';
+            lines[pos.y][pos.x] = '.';
+            pos = (xx, yy);
+            checkValid(lines, linesBackup, dir);   
+            return true;
+        } else {
+            for(int i=0; i<lines.Length; i++)
+                lines[i]=linesBackup[i]; 
+            return false;
+        }
+    }
+    
+    if ((lines[yy][xx] == '[' && lines[yy][xx+targetSO] == ']') || (lines[yy][xx] == ']' && lines[yy][xx+targetSO] == '['))
+    {
+        if (targetSO==0)
+        {
+            for(int i=0; i<lines.Length; i++)
+                Console.WriteLine(lines[i]);
+            Console.WriteLine("Error. pushSO=0.");
+        }
+        (int x, int y) dummy = (xx,yy), dummy2 = (xx+mySO,yy);
+
+    }
+    return false;
+    //lines[pos.y][pos.x]='!';
+    //printLines(lines, dir);
+    //throw new NotImplementedException($"Unhandled {pos.y},{pos.x} myso:{mySO} dir:{dir} target box:" + lines[yy][xx] + lines[yy][xx+mySO]);
+}
+
+void checkValid(char[][] lines, char[][] backupLines, (int x, int y) dir)
+{
+    for(int y=0; y<lines.Length; y++)
+    {
+        if (lines[y].Length==0)
+            break;
+        for(int x=0; x<lines[y].Length; x++)
+        {
+            if ((lines[y][x]=='[' && lines[y][x+1]!=']') || (lines[y][x]==']' && lines[y][x-1]!='['))
+            {
+                for(int yy=0; yy<lines.Length; yy++) {
+                    Console.Write(lines[yy]); Console.WriteLine(backupLines[yy]);
+                    if (lines[yy].Length==0)
+                        break;
+                }
+                Console.WriteLine("Validation error. Dir:" + dir);
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
+
+
+void printLines(char[][] lines, (int x, int y) dir)
+{
+    for(int y=0; y<lines.Length; y++)
+    {
+        if (lines[y].Length==0)
+            break;
+        Console.WriteLine(lines[y]);
+    }
+}
---
+++ day16/Program.cs
@@ -0,0 +1,194 @@
+﻿/*
+    Görkem Paçacı
+    AoC24 day 16
+*/
+using System.ComponentModel;
+using System.Diagnostics;
+
+
+Stopwatch sp = new Stopwatch();
+sp.Start();
+part1();
+sp.Stop();
+Console.WriteLine("time: " + sp.ToString());
+sp.Restart();
+part2();
+sp.Stop();
+Console.WriteLine("time:" + sp.ToString());
+
+void part1()
+{
+    var lines = File.ReadAllLines("input.txt");
+    var lines2 = lines.Select(l => l.ToCharArray()).ToArray();
+    (int x, int y) E = (-1, -1); bool foundE = false;
+    (int x, int y) S = (-1, -1); bool foundS = false;
+    for (int y = 0; y < lines.Length; y++)
+    {
+
+        if (!foundE)
+        {
+            int ee = lines[y].IndexOf('E');
+            if (ee > -1)
+            {
+                E = (ee, y);
+                foundE = true;
+            }
+        }
+        if (!foundS)
+        {
+            int ss = lines[y].IndexOf('S');
+            if (ss > -1)
+            {
+                S = (ss, y);
+                foundS = true;
+            }
+        }
+        if (foundE&&foundS)
+            break;
+    }
+    Stack<((int x, int y) pos, (int x, int y) dir, int costSoFar)> heads = new();
+    Dictionary<(int x, int y), long> cache = new();
+    heads.Push((S, (1,0), 0));
+    cache.Add(S, 0);
+    List<long> successes=new();
+    while(heads.Count>0)
+    {
+        var head = heads.Pop();
+        if (head.pos==E)
+        {
+            successes.Add(head.costSoFar);
+            continue;
+        }
+        var around = new (int x, int y)[] {(0, 1), (1, 0), (0, -1), (-1, 0)};
+        foreach(var move in around)
+        {
+            int xx=head.pos.x+move.x;
+            int yy=head.pos.y+move.y;
+            if (!(lines[yy][xx] == '.' || lines[yy][xx] == 'E'))
+            {
+                continue;
+            }
+            int newCost=head.costSoFar+1+turnCost(head.dir,move);
+            if (cache.TryGetValue((xx, yy), out var oldCost) && oldCost < newCost)
+            {
+                continue;
+            } else cache[(xx,yy)] = newCost;
+            heads.Push((pos:(xx, yy), dir:move, costSoFar:newCost));
+        }
+        
+        lines2[head.pos.y][head.pos.x] = 'O';
+    }
+    // for(int i=0; i<lines2.Length; i++)
+    //     Console.WriteLine(lines2[i]);
+    long min = successes.Min();
+    Console.WriteLine("Part 1:"+min);
+}
+
+
+void part2()
+{
+    // 5158 too high
+    var lines = File.ReadAllLines("input.txt");
+    var lines2 = lines.Select(l => l.ToCharArray()).ToArray();
+    (int x, int y) E = (-1, -1); bool foundE = false;
+    (int x, int y) S = (-1, -1); bool foundS = false;
+    for (int y = 0; y < lines.Length; y++)
+    {
+
+        if (!foundE)
+        {
+            int ee = lines[y].IndexOf('E');
+            if (ee > -1)
+            {
+                E = (ee, y);
+                foundE = true;
+            }
+        }
+        if (!foundS)
+        {
+            int ss = lines[y].IndexOf('S');
+            if (ss > -1)
+            {
+                S = (ss, y);
+                foundS = true;
+            }
+        }
+        if (foundE&&foundS)
+            break;
+    }
+    Stack<((int x, int y) pos, (int x, int y) dir, int costSoFar, List<(int x, int y)> visited)> heads = new();
+    Dictionary<(int x, int y), long> cache = new();
+    heads.Push((S, (1,0), 0, new()));
+    cache.Add(S, 0);
+    var bestSoFar = int.MaxValue;
+    List<(int x, int y)> onBestPathList = new();
+    while(heads.Count>0)
+    {
+        var head = heads.Pop();
+        if (head.pos==E)
+        {
+            if (head.costSoFar < bestSoFar)
+            {
+                bestSoFar = head.costSoFar;
+                onBestPathList = new(head.visited);
+            } else if (head.costSoFar == bestSoFar)
+            {
+                onBestPathList.AddRange(head.visited);
+            }
+            continue;
+        }
+        var around = new (int x, int y)[] {(0, 1), (1, 0), (0, -1), (-1, 0)};
+        foreach(var move in around)
+        {
+            int xx=head.pos.x+move.x;
+            int yy=head.pos.y+move.y;
+            if (!(lines[yy][xx] == '.' || lines[yy][xx] == 'E'))
+            {
+                continue;
+            }
+            int newCost=head.costSoFar+1+turnCost(head.dir,move);
+            if (cache.TryGetValue((xx, yy), out var oldCost) && oldCost < newCost - 1000)
+            {
+                continue;
+            } else cache[(xx,yy)] = newCost;
+            var newList = new List<(int x, int y)>(head.visited);
+            newList.Add((xx,yy));
+            heads.Push((pos:(xx, yy), dir:move, costSoFar:newCost, visited:newList));
+        }
+        
+        //lines2[head.pos.y][head.pos.x] = 'O';
+    }
+    for(int i=0; i<lines2.Length; i++)
+        Console.WriteLine(lines2[i]);
+    Console.WriteLine("Part 2:"+(onBestPathList.ToHashSet().Count+1));
+}
+
+int turnCost((int x, int y) dir1, (int x, int y) dir2)
+{
+    if (dir1==dir2)
+        return 0;
+    (int x, int y)[] directions = new[] { (0, -1), (1, 0), (0, 1), (-1, 0) };
+    const int up=0, right=1, down=2, left=3;
+    int dirMin = Math.Min(Array.IndexOf(directions, dir1), Array.IndexOf(directions, dir2));
+    int dirMax = Math.Max(Array.IndexOf(directions, dir1), Array.IndexOf(directions, dir2));
+    return (dirMin, dirMax) switch
+    {
+        (up, right) => 1000,
+        (up, left) => 1000,
+        (up, down) => 2000,
+        (right, down) => 1000,
+        (right, left) => 2000,
+        (down, left) => 1000,
+        _ => throw new ArgumentException("turnCost")
+    };
+    // var dirs = new (int x, int y)[] {(0, -1), (1, 0), (0, 1), (-1, 0)};
+    // var dir1Index = Array.IndexOf(dirs, dir1);
+    // var dir2Index = Array.IndexOf(dirs, dir2);
+    // if (dir1Index < 0 || dir2Index < 0)
+    //     throw new ArgumentException("turnCost");
+    // var lo = Math.Min(dir1Index, dir2Index);
+    // var hi = Math.Max(dir1Index, dir2Index);
+    // var d = hi-lo % 3;
+    // return d*1000;
+}
+
